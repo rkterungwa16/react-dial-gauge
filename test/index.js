4a2a1b271e728501e5687379b443b999f6a441cf
richard_kombol <richard@ashnelsonpartners.com>
2018-09-25 13:36:31 +0100

    set up project

---
+++ test/index.js
@@ -0,0 +1,8 @@
+
+const value = 2
+const check = {
+  get (value) {
+    return value
+  }
+}
+check.get(value)
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ]
+}
